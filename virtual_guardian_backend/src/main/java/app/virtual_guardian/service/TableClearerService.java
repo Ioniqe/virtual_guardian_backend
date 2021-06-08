2a1ed858acb51edecf029db2f7711cfb3c0b996c
Bozdog Ioana <bozdogioana@yahoo.com>
2021-06-08 23:03:08 +0300

    Get ml variables at compile time

--- virtual_guardian_backend/src/main/java/app/virtual_guardian/service/TableClearerService.java
+++ virtual_guardian_backend/src/main/java/app/virtual_guardian/service/TableClearerService.java
@@ -2,7 +2,10 @@ package app.virtual_guardian.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class TableClearerService implements CommandLineRunner {
@@ -18,6 +21,13 @@ public class TableClearerService implements CommandLineRunner {
     public void run(String... args) throws Exception {
         //place your code here
         dayService.deleteAll();
+
+        final String uri = "http://localhost:5000/get_ml_variables";
+        HttpHeaders headers = new HttpHeaders(); //set Headers
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForEntity(uri, String.class);
     }
 
 }
